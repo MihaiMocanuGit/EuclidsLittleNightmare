4c0a056faa080c703b35ae815b857b0cd8753cf8
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-07-04 10:48:18 +0300

    Setup | Changed Project name and improved run shell script.
    repair

--- run.sh
+++ run.sh
@@ -3,16 +3,43 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ $# -eq 0 ]
-then 
-	build_type=""
-else
-	build_type="$1"
-fi
+project="EuclidsLittleNightmare"
 
-if [ "$build_type" != "debug" ]
+build_type="release"
+job_count="12"
+
+Help()
+{
+   echo "Builds and runs the project."
+   echo
+   echo "Syntax: run [-h|b|j]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "b     Sets the build type (debug/release/relwithdebinfo/minsizerel).
+         DEFAULT: release."
+   echo "j     Sets the thread job count.
+         DEFAULT: 12."
+   echo
+}
+
+while getopts ":hb:j:" option; do
+   case $option in
+      h) # help
+         Help
+         exit;;
+      b) # build type
+         build_type=$OPTARG;;
+      j) # job count
+         job_count=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+if ! [[ "$build_type" =~ ^(debug|relwithdebinfo|minsizerel)$ ]]
 then
-	build_type="release"
+    build_type="release"
 fi
 
 echo "build_type = $build_type"
@@ -21,7 +48,9 @@ build_dir="$(dirname $0)/build_$build_type"
 echo "build_dir = $build_dir"
 
 echo "Setting build options"
-time (cmake "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" -DCMAKE_BUILD_TYPE="$build_type" -S . -B "$build_dir" && echo "Building $build_type" && cmake --build "$build_dir" -j 16) &&
+time (cmake "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" \
+    -DCMAKE_BUILD_TYPE="$build_type" -S . -B "$build_dir" \
+    && echo "Building $build_type" && cmake --build "$build_dir" -j "$job_count") &&
 
-echo "Running $build_dir/EuclidsNightmare" &&
-"$build_dir/EuclidsNightmare"
+echo "Running $build_dir/$project" &&
+"$build_dir/$project"
