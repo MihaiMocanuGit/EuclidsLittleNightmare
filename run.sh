6574df574c76161ea7c2fd261fb6edea5473b368
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-07-15 21:05:07 +0300

    Setup | Option for run.sh to only build without a run.

--- run.sh
+++ run.sh
@@ -6,29 +6,33 @@ IFS=$'\n\t'
 project="EuclidsLittleNightmare"
 
 build_type="release"
+build_only=false
 job_count="12"
 
 Help()
 {
    echo "Builds and runs the project."
    echo
-   echo "Syntax: run [-h|b|j]"
+   echo "Syntax: run [-h|b|B|j]"
    echo "options:"
    echo "h     Print this Help."
    echo "b     Sets the build type (debug/release/relwithdebinfo/minsizerel).
          DEFAULT: release."
+   echo "B     Build only, do not run"
    echo "j     Sets the thread job count.
          DEFAULT: 12."
    echo
 }
 
-while getopts ":hb:j:" option; do
+while getopts ":hb:Bj:" option; do
    case $option in
       h) # help
          Help
          exit;;
       b) # build type
          build_type=$OPTARG;;
+      B) # build only
+         build_only=true;;
       j) # job count
          job_count=$OPTARG;;
      \?) # Invalid option
@@ -52,5 +56,7 @@ time (cmake "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" \
     -DCMAKE_BUILD_TYPE="$build_type" -S . -B "$build_dir" \
     && echo "Building $build_type" && cmake --build "$build_dir" -j "$job_count") &&
 
-echo "Running $build_dir/$project" &&
-"$build_dir/$project"
+if [ "$build_only" = false ]
+then
+    echo "Running $build_dir/$project" && "$build_dir/$project"
+fi
