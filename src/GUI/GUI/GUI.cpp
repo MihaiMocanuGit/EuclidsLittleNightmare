62735db86dfffaac9b83519bf42961e925533b7a
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-07-20 22:01:10 +0300

    BVH | Experimental live rendering.

--- src/GUI/GUI/GUI.cpp
+++ src/GUI/GUI/GUI.cpp
@@ -80,7 +80,7 @@ bool GUI::init()
     ImGuiIO &io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look
     // identical to regular ones.
