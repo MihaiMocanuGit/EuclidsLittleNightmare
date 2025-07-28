6132f63d14f229f789e9219485971757f283c767
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-07-28 21:56:53 +0300

    Experimental | Computing ray intersection angle.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Eigen/Geometry"
+#include "Eigen/src/Core/Matrix.h"
 #include "GUI/GUI.hpp"
 #include "Scene/TreeBVH/TreeBVH.hpp"
 #include "Utils/utils.hpp"
@@ -7,16 +8,51 @@
 #include <atomic>
 #include <cmath>
 #include <execution>
-#include <iostream>
-#include <numeric>
+#include <numbers>
 #include <random>
 
 // CAUTION - ENTER AT YOUR OWN RISK!
 //
-// The current state of main.cpp file represents a sandbox like environment for experimenting with
-// different concepts. This is not the "final application".
+// The current state of main.cpp file represents a sandbox like environment used for experimenting
+// with different concepts. This is not the "final application".
 //
-// No recommended coding practices are followed in this wild west.
+// No recommended coding practices are followed in this godless place.
+//
+// Rest at this bonfire to replenish your strength before your adventure
+//
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣯⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⢺⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⣧⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢶⣿⣋⣟⠭⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣭⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⢮⣳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡿⣦⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠢⣽⣅⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆⢤⣿⡇⠀⠀⠀⠀⣸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠸⣞⡇⠀⠀⠀⠀⡏⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⣿⣷⠀⠀⠀⠀⢻⡈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⢸⣿⡆⠀⠀⠀⠀⢳⣬⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡈⣿⣧⠀⠀⢠⡄⣸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⢹⣿⡀⠀⢸⢧⠟⢹⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠸⣿⡇⣠⠋⢾⣾⢸⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡖⠀⠸⣿⣶⣿⣷⡏⢰⡿⢿⠏⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⣴⢋⣿⣿⣿⠇⡟⠁⣏⠀⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡞⣏⢦⠇⢸⡿⢿⠋⢀⣤⣀⡘⢦⡟⢸⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⠃⢀⣴⡆⠀⠀⠈⣹⣿⡷⠆⠀⣧⠈⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠜⢁⡴⠋⡀⠙⢄⠀⣰⣿⣟⠓⠀⠀⢉⣴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⢰⡏⡠⠊⢀⡴⣇⠀⢀⡞⠉⠛⠀⡀⢀⣄⣩⠌⠙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⢣⠶⠖⠊⢀⣈⠉⣹⡷⢀⣴⡯⠔⣛⡵⠁⣠⡏⠸⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⢹⡿⢿⠟⠀⣰⡞⠉⣿⡷⠇⠃⣠⢴⣶⣾⡋⢀⡴⣽⠁⠀⠘⣏⣀⢰⣆⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⣠⣶⣶⣅⣠⣶⠀⠒⠟⢁⡴⠋⠀⠀⠀⢹⣿⣿⡋⣧⢸⡇⡏⣀⣀⠀⠙⣿⣉⠙⢤⡄⠀⠀⠀
+// ⠀⠀⣠⣴⣺⢿⣿⣿⡛⠛⠿⠿⣯⣷⡲⣶⣟⣻⡀⠀⣠⣿⣿⣖⣸⣨⣿⠿⠛⣻⣿⣶⣾⣾⠇⠀⠻⣄⠀⠀
+// ⠀⣾⢟⠿⠿⢶⣮⡙⢏⢢⡀⢠⡌⣿⣿⡿⠟⡿⢳⣼⣿⣿⣿⣾⣿⣧⣤⣤⣤⣿⣿⣭⣿⠁⠀⠀⣀⣈⣧⠀
+// ⢺⣥⢿⠾⠿⠿⠿⡿⠚⢋⣠⠯⣿⢉⢉⠻⠾⠛⢿⣿⠻⠿⢛⢋⣤⣯⣭⠽⠶⣾⣻⢿⣻⢿⠶⢛⣻⡿⢽⠄
+//
+// Don't you dare go Hollow!
 
 std::array<float, 3> vectorField(std::array<float, 3> x)
 {
@@ -31,7 +67,7 @@ std::array<float, 3> vectorField(std::array<float, 3> x)
     return v;
 }
 
-struct Elipsoid_t
+struct Ellipsoid_t
 {
     Eigen::Vector3f center;
     Eigen::Vector3f coeff;
@@ -40,9 +76,71 @@ struct Elipsoid_t
     constexpr bool checkHit(const Eigen::Vector3f &position) const noexcept
     {
         float coeffSum {0};
-        for (unsigned dim {0}; dim < 3; ++dim)
+        for (unsigned dim {0u}; dim < 3u; ++dim)
             coeffSum += std::pow(position[dim] - center[dim], 2.0f) / std::pow(coeff[dim], 2.0f);
         return coeffSum <= 1.0f;
+    }
+
+    float hitAngle(const Eigen::Vector3f &position, const Eigen::Vector3f &direction) const noexcept
+
+    {
+        // Ellipsoid: (x-xc)^2/a^2 + (y-yc)^2/b^2 + (y-yc)^2/c^2 = 1
+        //
+        // Line: [x,y,z] = direction * t + position
+        //
+        // Intersection:
+        // (xd*t + xp - xc)^2/a^2 + (yd*t + yp - yc)^2/b^2 + (zd*t + zp - zc)^2/c^2 = 1
+        //
+        // Equation in t:
+        // t^2 * (xd^2/a^2 + yd^2/b^2 + zd^2/c^2)
+        // + t * (xd*(xp-xc)/a^2 + yd*(yp-yc)/b^2 + zd*(zp-zc)/c^2)
+        // + (xp-xc)^2/a^2 + (yp-yc)^2/b^2 + (zp-zc)^2/c^2 - 1 = 0
+        //
+        // Rewrite as: t^2 * A + t * B + C = 0
+        //
+        // Discriminant: delta = B^2 - 4*A*C
+        //
+        // If delta < 0 then the ray missed.
+        // Else if delta >= 0 then there exists at least one intersection point. Find the smallest
+        // root of t.
+        //
+        // Compute the gradient on the ellipsoid surface at the first intersection. This will yield
+        // a normal vector.
+        //
+        // Compute the cosine of the angle between the direction vector and normal vector. It can be
+        // easily computed from their dot product.
+
+        float A = 0;
+        for (unsigned dim {0u}; dim < 3u; ++dim)
+            A += std::powf(direction[dim], 2.0f) / std::powf(coeff[dim], 2.0f);
+
+        float B = 0;
+        for (unsigned dim {0u}; dim < 3u; ++dim)
+            B += std::powf(direction[dim] * (position[dim] - center[dim]), 2.0f) /
+                 std::powf(coeff[dim], 2.0f);
+
+        float C = 0;
+        for (unsigned dim {0u}; dim < 3u; ++dim)
+            C += std::powf(position[dim] - center[dim], 2.0f) / std::powf(coeff[dim], 2.0f);
+
+        float delta = std::powf(B, 2.0f) - 4 * A * C;
+        if (delta >= 0)
+        {
+            // It can be observed that t1 < t2 and we only care about the first one: t1
+            float t1 {(-B - std::sqrt(delta)) / (2 * A)};
+            Eigen::Vector3f intersect {direction * t1 + position};
+            Eigen::Vector3f gradient {
+                2 * intersect[0] / std::powf(coeff[0], 2.0f),
+                2 * intersect[1] / std::powf(coeff[1], 2.0f),
+                2 * intersect[2] / std::powf(coeff[2], 2.0f),
+            };
+            gradient.normalize();
+            Eigen::Vector3f dirVector {direction.normalized()};
+            float cosine {gradient.dot(dirVector) / (gradient.norm() * dirVector.norm())};
+            return cosine;
+        }
+
+        return 0.0f;
     }
 
     ELN::BVH::Boundary_t<float> boundary() const noexcept
@@ -62,8 +160,8 @@ int main()
     const ELN::BVH::Boundary_t<float> WORLD_BD {
         1.0f * Eigen::Vector3f {-1.0f * WIDTH, -1.0f * HEIGHT, 0.0f},
         1.0f * Eigen::Vector3f {WIDTH, HEIGHT, HEIGHT}};
-    constexpr size_t NO_ELEMENTS = 500;
-    std::vector<ELN::BVH::CompactTree<Elipsoid_t>::ElemBdrPair> elements;
+    constexpr size_t NO_ELEMENTS = 1000;
+    std::vector<ELN::BVH::CompactTree<Ellipsoid_t>::ElemBdrPair> elements;
     elements.reserve(NO_ELEMENTS);
     std::mt19937 gen;
     for (size_t i {0}; i < NO_ELEMENTS; ++i)
@@ -73,12 +171,21 @@ int main()
         const auto color = [&]()
         {
             std::uniform_int_distribution<uint8_t> distrib(64, 255);
-            return std::array<uint8_t, 3> {distrib(gen), distrib(gen), distrib(gen)};
+            Eigen::Vector3f colorVec(distrib(gen), distrib(gen), distrib(gen));
+            colorVec.normalize();
+            colorVec *= 255;
+
+            std::array<uint8_t, 3> byteColor;
+            for (unsigned dim = 0; dim < 3; ++dim)
+                byteColor[dim] = std::clamp(static_cast<int>(colorVec[dim]), 0, 255);
+
+            return byteColor;
         };
-        Elipsoid_t object {.center = WORLD_BD.sample(), .coeff = OBJ_BD.sample(), .color = color()};
+        Ellipsoid_t object {
+            .center = WORLD_BD.sample(), .coeff = OBJ_BD.sample(), .color = color()};
         elements.emplace_back(object, object.boundary());
     }
-    const ELN::BVH::CompactTree<Elipsoid_t> tree {std::move(elements)};
+    const ELN::BVH::CompactTree<Ellipsoid_t> tree {std::move(elements)};
 
     std::atomic_bool continue_running {true};
     ELN::GUI gui {continue_running};
@@ -166,7 +273,15 @@ int main()
                                 stepsize = preciseStepSize;
                                 if (boundary.get().checkHit({x, y, z}))
                                 {
-                                    setColorPixel(boundary.get().color);
+                                    auto pixelColor = boundary.get().color;
+                                    float cosOfAngle {boundary.get().hitAngle(
+                                        {x, y, z},
+                                        {rayDirVector[0], rayDirVector[1], rayDirVector[2]})};
+                                    pixelColor[0] *= cosOfAngle;
+                                    pixelColor[1] *= cosOfAngle;
+                                    pixelColor[2] *= cosOfAngle;
+
+                                    setColorPixel(pixelColor);
                                     hitObject = true;
                                     break;
                                 }
